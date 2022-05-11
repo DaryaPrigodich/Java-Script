bfd7d7b537dff604a7ff61156df0b4a75c7d584a
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2022-05-11 16:38:24 +0200

    Update while and for.js

--- while and for.js
+++ while and for.js
@@ -1,7 +1,28 @@
+// Циклы
+
+let rom = 6;
+while (rom) {
+    console.log ('hi-', rom);
+    rom--;    
+}
+
+let num = 9;    
+for (; num < 15; num++) {
+    console.log (num);
+    if (num == 13) break
+}
+
+let lon = 0;
+for (; lon < 5; lon++)  {
+    if (lon == 2) continue;
+    console.log(lon);
+}
+
+
 let dad = 0
 while (dad < 5) {
     console.log(dad + 'dasha')
-dad++
+    dad++
 }
 
 
@@ -9,12 +30,11 @@ dad++
 for (let temperature = 0; temperature < 10; temperature++) {
     if (temperature < 3) {
         console.log('temperature = ', temperature  ,'cold')
-    } else if (temperature >= 3 && temperature < 7) {
+  } else if (temperature >= 3 && temperature < 7) {
         console.log('temperature = ', temperature  ,'good')
-    }  else {
+ }  else {
         console.log('temperature = ', temperature  ,'hot')
-    } 
-}
+ }}
 
 
 
